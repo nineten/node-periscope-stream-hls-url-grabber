b19da43e53bd2c90b6c0be808bf1a82d529272c0
nineten <jayden87@gmail.com>
2016-01-08 14:08:49 +0800

    Fixed no url match in tweet error

--- hlsUrlGrabber.js
+++ hlsUrlGrabber.js
@@ -18,27 +18,33 @@ PeriscopeHlsUrlGrabber.prototype.start = function(userId, callback) {
 			} else {
 				var tweet = tweets[0].text;
 				var uriPattern = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[{};:'".,<>?«»“”‘’]|\]|\?))/ig;
-				var tweetUrl = tweet.match(uriPattern)[0];
+				var urlMatch = tweet.match(uriPattern);
 
-				urlExpander.expand(tweetUrl, function(err, longUrl) {
-						console.log(longUrl);
-						periscope(longUrl, function(err, details) {
-							var status;
-							var hlsUrl;
+				if (urlMatch === null) {
+					callback("There is no stream", null);
+				} else {
+					var tweetUrl = uriMatch[0];
 
-							if (!err) {
-								if (details.type === "StreamTypeWeb") {
-									hlsUrl = details.https_hls_url;
-								}	else {
-									status = "Stream ended";
+					urlExpander.expand(tweetUrl, function(err, longUrl) {
+							console.log(longUrl);
+							periscope(longUrl, function(err, details) {
+								var status;
+								var hlsUrl;
+
+								if (!err) {
+									if (details.type === "StreamTypeWeb") {
+										hlsUrl = details.https_hls_url;
+									}	else {
+										status = "Stream ended";
+									}
+								} else {
+									status = "There is no stream";
 								}
-							} else {
-								status = "There is no stream";
-							}
 
-							callback(status, hlsUrl);
-						});
-				});
+								callback(status, hlsUrl);
+							});
+					});
+				}
 			}
 		} else {
 			console.log(this.twitterClientConfig);
