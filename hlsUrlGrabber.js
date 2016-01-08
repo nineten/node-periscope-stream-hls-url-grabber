628d7fed54da4a20d5fe2a5728d33325ee31e298
nineten <jayden87@gmail.com>
2016-01-08 14:05:40 +0800

    fixed no tweet error

--- hlsUrlGrabber.js
+++ hlsUrlGrabber.js
@@ -8,39 +8,38 @@ function PeriscopeHlsUrlGrabber(username, config) {
 }
 
 PeriscopeHlsUrlGrabber.prototype.start = function(userId, callback) {
-	var streamId;
-	var streamAddress = 'https://www.periscope.tv/w/' + streamId;
-
-	console.log(this.twitterClientConfig);
 	var params = {screen_name: this.username};
 	var client = new Twitter(this.twitterClientConfig);	
 
-	console.log(client);
 	client.get('statuses/user_timeline', params, function(error, tweets, response){
 		if (!error) {
-			var tweet = tweets[0].text;
-			var uriPattern = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[{};:'".,<>?«»“”‘’]|\]|\?))/ig;
-			var tweetUrl = tweet.match(uriPattern)[0];
+			if (tweets === null) {
+				callback("There is no stream", null);
+			} else {
+				var tweet = tweets[0].text;
+				var uriPattern = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[{};:'".,<>?«»“”‘’]|\]|\?))/ig;
+				var tweetUrl = tweet.match(uriPattern)[0];
 
-			urlExpander.expand(tweetUrl, function(err, longUrl) {
-					console.log(longUrl);
-					periscope(longUrl, function(err, details) {
-						var status;
-						var hlsUrl;
+				urlExpander.expand(tweetUrl, function(err, longUrl) {
+						console.log(longUrl);
+						periscope(longUrl, function(err, details) {
+							var status;
+							var hlsUrl;
 
-						if (!err) {
-							if (details.type === "StreamTypeWeb") {
-								hlsUrl = details.https_hls_url;
-							}	else {
-								status = "Stream ended";
+							if (!err) {
+								if (details.type === "StreamTypeWeb") {
+									hlsUrl = details.https_hls_url;
+								}	else {
+									status = "Stream ended";
+								}
+							} else {
+								status = "There is no stream";
 							}
-						} else {
-							status = "There is no stream";
-						}
 
-						callback(status, hlsUrl);
-					});
-			});
+							callback(status, hlsUrl);
+						});
+				});
+			}
 		} else {
 			console.log(this.twitterClientConfig);
 			console.log(error);
